b07a631ff4b37da8fb9c9ce4d1e3fafe0608b066
cruzmenezes <cruz.menezes@outlook.com>
2025-07-27 17:34:54 -0300

    upload spring boot

---
+++ apiusuario/src/test/java/com/edson/apiusuario/ApiusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.apiusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
